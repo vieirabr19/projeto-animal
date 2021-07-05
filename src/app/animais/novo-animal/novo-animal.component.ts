c786afb49e2220f83fa8dd96e3da0481872b8b3a
Luiz Vieira <luiz22_sp@hotmail.com>
2021-07-05 07:44:23 -0300

    Formulario novo animal

--- src/app/animais/novo-animal/novo-animal.component.ts
+++ src/app/animais/novo-animal/novo-animal.component.ts
@@ -1,5 +1,9 @@
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
+import { AnimaisService } from '../animais.service';
 
 @Component({
   selector: 'app-novo-animal',
@@ -12,18 +16,43 @@ export class NovoAnimalComponent implements OnInit {
   preview!: string;
   percentualConcluido = 0;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private animaisService: AnimaisService
+    ) { }
 
   ngOnInit(): void {
     this.formComentario = this.fb.group({
-      file: [null, [Validators.maxLength(30)]]
+      file: [null, [Validators.required]],
+      description: [null, [Validators.maxLength(300)]],
+      allowComments: [null],
     })
   }
 
-  upload(){}
+  upload(){
+    const allowComments = this.formComentario.get('allowComments')?.value ?? false;
+    const description = this.formComentario.get('description')?.value ?? '';
+
+    this.animaisService.upload(description, allowComments, this.file)
+    .pipe(finalize(() => this.router.navigateByUrl('/animais')))
+    .subscribe(
+      (event: HttpEvent<any>) => {
+        if(event.type === HttpEventType.UploadProgress){
+          const total = event.total ?? 1;
+          this.percentualConcluido = Math.round(100 * (event.loaded / total))
+        }
+      },
+      error => console.log(error)
+    );
+  }
 
   gravarArquivo(arquivo: any){
-    console.log(arquivo)
+    const [file] = arquivo?.files;
+    this.file = file;
+    const reader = new FileReader();
+    reader.onload = (event: any) => this.preview = event.target.result;
+    reader.readAsDataURL(file);
   }
 
 }
